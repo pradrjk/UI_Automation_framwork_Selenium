20d72cfcd3a548ead9b5ac7d718d06584579e9d0
pradeepmac <pradrjk@gmail.com>
2024-05-04 11:30:25 +1000

    Added screenshot capture in BaseTest.class and add assertions in TC001.java class

--- src/test/java/baseClass/BaseTest.java
+++ src/test/java/baseClass/BaseTest.java
@@ -1,5 +1,8 @@
 package baseClass;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -7,6 +10,7 @@ import org.testng.annotations.BeforeSuite;
 import utils.ReadConfig;
 import utils.BrowserFactory;
 
+import java.io.File;
 import java.io.IOException;
 
 
@@ -30,6 +34,21 @@ public class BaseTest {
     public void tearDown()
     {
         BrowserFactory.quitBrowser(driver);
+    }
+
+    public void captureScreenShot(WebDriver driver,String testName) throws IOException
+    {
+        // Convert webdriver object to TakesScreenshot interface
+        TakesScreenshot screenshot= ((TakesScreenshot)driver);
+
+        // Step 2 :call getScreenshotAs method to capture image file
+
+        File src= screenshot.getScreenshotAs(OutputType.FILE);
+        File srcpath=new File("."+"//Screenshots//"+ testName + ".png");
+
+        // Step 3 : copy image file to destination
+        FileUtils.copyFile(src, srcpath);
+
     }
 
 }
--- src/test/java/testCases/TC001.java
+++ src/test/java/testCases/TC001.java
@@ -2,19 +2,33 @@ package testCases;
 
 import baseClass.BaseTest;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.LoginPage;
+
+import java.io.IOException;
 
 public class TC001 extends BaseTest {
 
-
-
-
     @Test(priority =1)
-    public void verifyLogin()
-    {
-        driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys(BaseTest.config.getUsername());
-        driver.findElement(By.id("password")).sendKeys(BaseTest.config.getPassword());
-        driver.findElement(By.id("login-button")).click();
+    public void verifyLogin() throws IOException {
+        LoginPage lp = new LoginPage(driver);
+        String uname = config.getUsername();
+        String pwd = config.getPassword();
+
+        lp.loginIn(uname, pwd);
+
+        if(uname.equals("standard_user") && pwd.equals("secret_sauce"))
+        {
+            System.out.println("Test Passed");
+            Assert.assertTrue(true);
+        }
+        else
+        {
+            captureScreenShot(driver,"VerifyLogin");
+            Assert.assertTrue(false);
+        }
+
 
 
     }
