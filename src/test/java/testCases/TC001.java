8ffbb4bde40831915a82db5a70c294a5dbd4a888
pradeepmac <pradrjk@gmail.com>
2024-05-05 09:42:04 +1000

    Added new test cases to test shopping cart bubble

--- src/test/java/testCases/TC001.java
+++ src/test/java/testCases/TC001.java
@@ -2,11 +2,15 @@ package testCases;
 
 import baseClass.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.IOException;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class TC001 extends BaseTest {
 
@@ -26,12 +30,54 @@ public class TC001 extends BaseTest {
         else
         {
             captureScreenShot(driver,"VerifyLogin");
-            Assert.assertTrue(false);
+            Assert.fail();
         }
 
 
 
     }
+    @Test(priority =2)
+    public void fetchDashboardText() throws IOException
+    {
 
+        String dashboardTitle = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[1]/div[2]/div")).getText();
+        String actualTitle= "Swag Labs";
+
+        if(actualTitle.equals(dashboardTitle))
+        {
+            System.out.println("Test Passed");
+        }
+        else{
+            captureScreenShot(driver,"fetchDashboardText");
+
+        }
+        assertEquals(dashboardTitle , actualTitle);
+
+    }
+
+    @Test(priority = 3)
+    public void addItemToCart() throws InterruptedException, IOException {
+
+        HomePage hp = new HomePage(driver);
+        hp.AddItemToCart();
+
+        WebElement ShoppingCartWithItem = driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span"));
+
+        int ItemCount = Integer.parseInt(ShoppingCartWithItem.getText());
+        if(ItemCount == 1)
+        {
+            System.out.println("Test Passed and Item added. Item count is "+ItemCount);
+        }
+        else {
+            captureScreenShot(driver,"addItemToCart");
+        }
+
+    }
+
+    @Test(priority = 4)
+    public void removeItem()
+    {
+
+    }
 
 }
